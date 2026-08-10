a6b4ad5eef05a22406d04bd36c5c0d14f08f23bb
2nd-Bird <134070107+2nd-Bird@users.noreply.github.com>
2026-08-10 17:42:21 +0900

    Update PWA cache for v0.8 modules

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='bebop-reader-v7';
-const CORE=['./','./index.html','./styles.css','./pitch-guide.css','./app.js','./manifest.webmanifest','./assets/icon.svg','./src/exercises.js','./src/storage.js','./src/notation.js','./src/notation/layout.js','./src/audio.js','./src/mic.js','./src/pitchDetector.js','./src/music/pitch.js','./src/scoring.js','./src/pitchGuide.js','./src/uiEnhancements.js'];
+const CACHE='bebop-reader-v8';
+const CORE=['./','./index.html','./styles.css','./pitch-guide.css','./v08.css','./app-v08.js','./manifest.webmanifest','./assets/icon.svg','./src/exercises.js','./src/storage.js','./src/notation.js','./src/notation/layout.js','./src/notation/follow.js','./src/audio.js','./src/mic.js','./src/pitchDetector.js','./src/music/pitch.js','./src/scoring.js','./src/pitchGuide.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{
